d8334105c39f1487e8a61313e2ad3586b424c2f1
MarkusLange <mlange.unipb@googlemail.com>
2020-07-02 22:31:53 +0200

    Add support for Teensy 4.x
    Add support for Teensy 4.x and rewrite some parts for the use of Teensy 3.6 Serial 6
    
    Experimental Timeout for Lin.read

--- src/lin_bus.cpp
+++ src/lin_bus.cpp
@@ -12,8 +12,11 @@ LIN::LIN(HardwareSerial* stream, uint16_t baudrate, uint8_t break_characters) {
   interbytespace = interbytedelay*Tbit;
   syncfieldPIDinterbytespace = syncfieldPIDinterbytedelay*Tbit;
   breakfieldinterbytespace = breakfieldinterbytedelay*Tbit;
+  response_nominalspace = response_nominal*Tbit;
+  response_maximalspace = response_nominalspace*response_max_factor;
   
   if (stream == &Serial1) {
+#if defined (__MK20DX128__) || defined(__MK20DX256__) || (__MKL26Z64__) || (__MK64FX512__) || (__MK66FX1M0__)
     PortRegister_C1 = &UART0_C1;
     PortRegister_C2 = &UART0_C2;
     PortRegister_C4 = &UART0_C4;
@@ -22,9 +25,15 @@ LIN::LIN(HardwareSerial* stream, uint16_t baudrate, uint8_t break_characters) {
 #ifdef HAS_KINETISK_UART0_FIFO
     UART0_RWFIFO = 1;
 #endif // HAS_KINETISK_UART0_FIFO
+#elif defined(__IMXRT1062__) // Teensy 4.0 & 4.1
+    PortRegister_LPUART_STAT = &LPUART1_STAT;
+    PortRegister_LPUART_BAUD = &LPUART1_BAUD;
+    PortRegister_LPUART_CTRL = &LPUART1_CTRL;
+#endif
   }
 
   if (stream == &Serial2) {
+#if defined (__MK20DX128__) || defined(__MK20DX256__) || (__MKL26Z64__) || (__MK64FX512__) || (__MK66FX1M0__)
     PortRegister_C1 = &UART1_C1;
     PortRegister_C2 = &UART1_C2;
     PortRegister_C4 = &UART1_C4;
@@ -33,102 +42,172 @@ LIN::LIN(HardwareSerial* stream, uint16_t baudrate, uint8_t break_characters) {
 #ifdef HAS_KINETISK_UART1_FIFO
     UART1_RWFIFO = 1;
 #endif // HAS_KINETISK_UART0_FIFO
+#elif defined(__IMXRT1062__) // Teensy 4.0 & 4.1
+    PortRegister_LPUART_STAT = &LPUART2_STAT;
+    PortRegister_LPUART_BAUD = &LPUART2_BAUD;
+    PortRegister_LPUART_CTRL = &LPUART2_CTRL;
+#endif
   }
   
   if (stream == &Serial3) {
+#if defined (__MK20DX128__) || defined(__MK20DX256__) || (__MKL26Z64__) || (__MK64FX512__) || (__MK66FX1M0__)
     PortRegister_C1 = &UART2_C1;
     PortRegister_C2 = &UART2_C2;
     PortRegister_C4 = &UART2_C4;
     PortRegister_S2 = &UART2_S2;
     PortRegister_BDH = &UART2_BDH;
+#elif defined(__IMXRT1062__) // Teensy 4.0 & 4.1
+    PortRegister_LPUART_STAT = &LPUART3_STAT;
+    PortRegister_LPUART_BAUD = &LPUART3_BAUD;
+    PortRegister_LPUART_CTRL = &LPUART3_CTRL;
+#endif
   }
 
-#if defined(__MK64FX512__) || defined(__MK66FX1M0__)
+#if defined (__MK64FX512__) || defined(__MK66FX1M0__) || defined(__IMXRT1062__)
   if (stream == &Serial4) {
+#if defined (__MK64FX512__) || defined(__MK66FX1M0__)
     PortRegister_C1 = &UART3_C1;
     PortRegister_C2 = &UART3_C2;
     PortRegister_C4 = &UART3_C4;
     PortRegister_S2 = &UART3_S2;
     PortRegister_BDH = &UART3_BDH;
+#elif defined(__IMXRT1062__) // Teensy 4.0 & 4.1
+    PortRegister_LPUART_STAT = &LPUART4_STAT;
+    PortRegister_LPUART_BAUD = &LPUART4_BAUD;
+    PortRegister_LPUART_CTRL = &LPUART4_CTRL;
+#endif
   }
   
   if (stream == &Serial5) {
+#if defined (__MK64FX512__) || defined(__MK66FX1M0__)
     PortRegister_C1 = &UART4_C1;
     PortRegister_C2 = &UART4_C2;
     PortRegister_C4 = &UART4_C4;
     PortRegister_S2 = &UART4_S2;
     PortRegister_BDH = &UART4_BDH;
+#elif defined(__IMXRT1062__) // Teensy 4.0 & 4.1
+    PortRegister_LPUART_STAT = &LPUART5_STAT;
+    PortRegister_LPUART_BAUD = &LPUART5_BAUD;
+    PortRegister_LPUART_CTRL = &LPUART5_CTRL;
+#endif
   }
   
   if (stream == &Serial6) {
+#if defined (__MK64FX512__)
     PortRegister_C1 = &UART5_C1;
     PortRegister_C2 = &UART5_C2;
     PortRegister_C4 = &UART5_C4;
     PortRegister_S2 = &UART5_S2;
     PortRegister_BDH = &UART5_BDH;
+#elif defined(__MK66FX1M0__)
+    PortRegister_LPUART_STAT = &LPUART0_STAT;
+    PortRegister_LPUART_BAUD = &LPUART0_BAUD;
+    PortRegister_LPUART_CTRL = &LPUART0_CTRL;
+#elif defined(__IMXRT1062__) // Teensy 4.0 & 4.1
+    PortRegister_LPUART_STAT = &LPUART6_STAT;
+    PortRegister_LPUART_BAUD = &LPUART6_BAUD;
+    PortRegister_LPUART_CTRL = &LPUART6_CTRL;
+#endif
   }
 #endif
 
-#if defined(__MK20DX128__) || defined(__MK20DX256__) // Teensy 3.0 & 3.1 & 3.2
+#if defined(__IMXRT1062__) // Teensy 4.0 & 4.1
+  if (stream == &Serial7) {
+    PortRegister_LPUART_STAT = &LPUART7_STAT;
+    PortRegister_LPUART_BAUD = &LPUART7_BAUD;
+    PortRegister_LPUART_CTRL = &LPUART7_CTRL;
+  }
+#endif
+
+#if defined(__IMXRT1062__) && defined(ARDUINO_TEENSY41) // Teensy 4.1
+  if (stream == &Serial8) {
+    PortRegister_LPUART_STAT = &LPUART8_STAT;
+    PortRegister_LPUART_BAUD = &LPUART8_BAUD;
+    PortRegister_LPUART_CTRL = &LPUART8_CTRL;
+  }
+#endif
+
+#if defined (__MK20DX128__) || defined(__MK20DX256__) // Teensy 3.0 & 3.1 & 3.2
   breaklength(break_characters);
-#elif defined(__MK64FX512__) // Teensy 3.5
+#elif defined (__MK64FX512__) // Teensy 3.5
   breaklength_35(break_characters);
-#elif defined(__MK66FX1M0__) // Teensy 3.6
+#elif defined (__MK66FX1M0__) // Teensy 3.6
   if (stream == &Serial6)
     breaklength_LP(break_characters);
   else
     breaklength_35(break_characters);
-#elif defined(__MKL26Z64__) // Teensy LC
+#elif defined (__MKL26Z64__) // Teensy LC
   breaklength_LC(break_characters, stream);
+#elif defined(__IMXRT1062__) // Teensy 4.0 & 4.1
+  breaklength_LP(break_characters);
 #endif
 }
 
 void LIN::breaklength_LP(uint8_t length) {
   switch (length) {
+#if defined(__IMXRT1062__) // Teensy 4.0 & 4.1
+    case  9:
+      //  9 Bits transmitted
+      (*PortRegister_LPUART_STAT) &= ~LPUART_STAT_BRK13;
+      (*PortRegister_LPUART_BAUD) &= ~LPUART_BAUD_SBNS;
+      (*PortRegister_LPUART_CTRL) &= ~LPUART_CTRL_M;
+      (*PortRegister_LPUART_BAUD) &= ~LPUART_BAUD_M10;
+      (*PortRegister_LPUART_CTRL) |= LPUART_CTRL_M7;
+#endif
     case 10:
       // 10 Bits transmitted
-      LPUART0_STAT &= ~LPUART_STAT_BRK13;
-      LPUART0_BAUD &= ~LPUART_BAUD_SBNS;
-      LPUART0_CTRL &= ~LPUART_CTRL_M;
-      LPUART0_BAUD &= ~LPUART_BAUD_M10;
+      (*PortRegister_LPUART_STAT) &= ~LPUART_STAT_BRK13;
+      (*PortRegister_LPUART_BAUD) &= ~LPUART_BAUD_SBNS;
+      (*PortRegister_LPUART_CTRL) &= ~LPUART_CTRL_M;
+      (*PortRegister_LPUART_BAUD) &= ~LPUART_BAUD_M10;
+#if defined(__IMXRT1062__) // Teensy 4.0 & 4.1
+      (*PortRegister_LPUART_CTRL) &= ~LPUART_CTRL_M7;
+#endif
       break;
     case 11:
       // 11 Bits transmitted
-      LPUART0_STAT &= ~LPUART_STAT_BRK13;
-      LPUART0_BAUD |= LPUART_BAUD_SBNS;
-      LPUART0_CTRL &= ~LPUART_CTRL_M;
-      LPUART0_BAUD &= ~LPUART_BAUD_M10;
+      (*PortRegister_LPUART_STAT) &= ~LPUART_STAT_BRK13;
+      (*PortRegister_LPUART_BAUD) |= LPUART_BAUD_SBNS;
+      (*PortRegister_LPUART_CTRL) &= ~LPUART_CTRL_M;
+      (*PortRegister_LPUART_BAUD) &= ~LPUART_BAUD_M10;
+#if defined(__IMXRT1062__) // Teensy 4.0 & 4.1
+      (*PortRegister_LPUART_CTRL) &= ~LPUART_CTRL_M7;
+#endif
       break;
     case 12:
       // 12 Bits transmitted
-      LPUART0_STAT &= ~LPUART_STAT_BRK13;
-      LPUART0_BAUD |= LPUART_BAUD_SBNS;
-      LPUART0_CTRL |= LPUART_CTRL_M;
-      LPUART0_BAUD &= ~LPUART_BAUD_M10;
+      (*PortRegister_LPUART_STAT) &= ~LPUART_STAT_BRK13;
+      (*PortRegister_LPUART_BAUD) |= LPUART_BAUD_SBNS;
+      (*PortRegister_LPUART_CTRL) |= LPUART_CTRL_M;
+      (*PortRegister_LPUART_BAUD) &= ~LPUART_BAUD_M10;
       break;
     case 13:
       // 13 Bits transmitted
-      LPUART0_STAT |= LPUART_STAT_BRK13;
-      LPUART0_BAUD &= ~LPUART_BAUD_SBNS;
-      LPUART0_CTRL &= ~LPUART_CTRL_M;
-      LPUART0_BAUD &= ~LPUART_BAUD_M10;
+      (*PortRegister_LPUART_STAT) |= LPUART_STAT_BRK13;
+      (*PortRegister_LPUART_BAUD) &= ~LPUART_BAUD_SBNS;
+      (*PortRegister_LPUART_CTRL) &= ~LPUART_CTRL_M;
+      (*PortRegister_LPUART_BAUD) &= ~LPUART_BAUD_M10;
+#if defined(__IMXRT1062__) // Teensy 4.0 & 4.1
+      (*PortRegister_LPUART_CTRL) &= ~LPUART_CTRL_M7;
+#endif
       break;
     case 14:
       // 14 Bits transmitted
-      LPUART0_STAT |= LPUART_STAT_BRK13;
-      LPUART0_BAUD &= ~LPUART_BAUD_SBNS;
-      LPUART0_CTRL |= LPUART_CTRL_M;
-      LPUART0_BAUD &= ~LPUART_BAUD_M10;
+      (*PortRegister_LPUART_STAT) |= LPUART_STAT_BRK13;
+      (*PortRegister_LPUART_BAUD) &= ~LPUART_BAUD_SBNS;
+      (*PortRegister_LPUART_CTRL) |= LPUART_CTRL_M;
+      (*PortRegister_LPUART_BAUD) &= ~LPUART_BAUD_M10;
       break;
     case 15:
       // 15 Bits transmitted
-      LPUART0_STAT |= LPUART_STAT_BRK13;
-      LPUART0_BAUD |= LPUART_BAUD_SBNS;
-      LPUART0_BAUD |= LPUART_BAUD_M10;
+      (*PortRegister_LPUART_STAT) |= LPUART_STAT_BRK13;
+      (*PortRegister_LPUART_BAUD) |= LPUART_BAUD_SBNS;
+      (*PortRegister_LPUART_BAUD) |= LPUART_BAUD_M10;
       break;
   }
 }
 
+#if defined (__MK20DX128__) || defined(__MK20DX256__) || (__MKL26Z64__) || (__MK64FX512__) || (__MK66FX1M0__)
 void LIN::breaklength_35(uint8_t length) {
   switch (length) {
     case 10:
@@ -270,6 +349,7 @@ void LIN::breaklength(uint8_t length) {
       break;
   }
 }
+#endif
 
 int LIN::addrParity(int PID) {
   int P0 = ((PID>>0) + (PID>>1) + (PID>>2) + (PID>>4)) & 1;
@@ -278,7 +358,7 @@ int LIN::addrParity(int PID) {
 }
 
 //sum = 0 LIN 1.X CRC, sum = PID LIN 2.X CRC Enhanced
-volatile byte LIN::dataChecksum (volatile byte* message, int length, uint16_t sum) {
+volatile byte LIN::dataChecksum(volatile byte* message, int length, uint16_t sum) {
   for (int i=0; i<length; i++) {
     sum += message[i];
     
@@ -292,20 +372,43 @@ void LIN::send_break() {
   // Toggle SBK to send Break
   //UART0_C2 |= UART_C2_SBK;
   //UART0_C2 &= ~UART_C2_SBK;
+  
+#if defined(__MK66FX1M0__) // Teensy 3.6
+  if (_stream == &Serial6) {
+    (*PortRegister_LPUART_CTRL) |= LPUART_CTRL_SBK;
+    (*PortRegister_LPUART_CTRL) &= ~LPUART_CTRL_SBK;
+  } else {
+	(*PortRegister_C2) |= UART_C2_SBK;
+    (*PortRegister_C2) &= ~UART_C2_SBK;
+  }
+#elif defined (__MK20DX128__) || defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MKL26Z64__) || defined(__MK66FX1M0__) // Teensy 3.0 & 3.1 & 3.2 & 3.5
+  (*PortRegister_C2) |= UART_C2_SBK;
+  (*PortRegister_C2) &= ~UART_C2_SBK;
+#elif defined(__IMXRT1062__) // Teensy 4.0 & 4.1
+  (*PortRegister_LPUART_CTRL) |= LPUART_CTRL_SBK;
+  (*PortRegister_LPUART_CTRL) &= ~LPUART_CTRL_SBK;
+#endif
 
-#if defined(__MK20DX128__) || defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MKL26Z64__) // Teensy 3.0 & 3.1 & 3.2 & 3.5 LC
+/*
+#if defined (__MK20DX128__) || defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MKL26Z64__) // Teensy 3.0 & 3.1 & 3.2 & 3.5
   (*PortRegister_C2) |= UART_C2_SBK;
   (*PortRegister_C2) &= ~UART_C2_SBK;
 #elif defined(__MK66FX1M0__) // Teensy 3.6
-  if (stream == &Serial6) {
-    LPUART0_CTRL |= LPUART_CTRL_SBK
-    LPUART0_CTRL &= ~LPUART_CTRL_SBK
+//  #ifdef HAS_KINETISK_UART5
+  if (LPU == 1) {
+    LPUART0_CTRL |= LPUART_CTRL_SBK;
+    LPUART0_CTRL &= ~LPUART_CTRL_SBK;
+	#pragma "Using KINETISK_LPUART0"
   } else {
+//  #else
     (*PortRegister_C2) |= UART_C2_SBK;
     (*PortRegister_C2) &= ~UART_C2_SBK;
+	#pragma "Using all other"
+//  #endif
   }
 #endif
-  
+*/
+
   _stream->write(SYNC);      //Sync 0x55
   _stream->flush();
   
@@ -359,14 +462,38 @@ int LIN::read(byte PID, byte* data, int length, int checksumtype) {
   _stream->write(send_pid);
   _stream->flush();
   //Serial.println("-----------------------");
+    
+/*
+  unsigned long actuall = micros();
   
-  while(i < (length+4)) {
-    if (_stream->available()) {
+  while ( i < (length+4) ) {
+    if ( _stream->available() ) {
+      tmp[i] = _stream->read();
+      Serial.println(tmp[i], HEX);
+      i++;
+    }
+	if ( (actuall+response_maximalspace) > micros() ) {
+	  break;
+	}
+  }
+   */
+  
+  elapsedMicros waiting;
+  
+  //Serial.println(response_maximalspace);
+  //Serial.println(waiting);
+  while ( i < (length+4) ) {
+    if ( _stream->available() ) {
       tmp[i] = _stream->read();
       //Serial.println(tmp[i], HEX);
       i++;
     }
+    if ( response_maximalspace < waiting ) {
+	  //Serial.println(waiting);
+	  break;
+	}
   }
+  //Serial.println(waiting);
   
   for (int i=3; i<length+3; i++)
 	data[i-3] = tmp[i];
--- src/lin_bus.h
+++ src/lin_bus.h
@@ -14,19 +14,31 @@ public:
   //Tbit Response
   #define responsedelay               8
   #define interbytedelay              0
+  //LIN supports from 2-8bit Data and an additional CRC bit
+  #define response_nominal     (8+1)*10
+  #define response_max_factor       1.4  
   
   unsigned long Tbit;
   int responsespace;
   int interbytespace;
   int syncfieldPIDinterbytespace;
   int breakfieldinterbytespace;
+  unsigned long response_nominalspace;
+  unsigned long response_maximalspace;
   uint8_t _breaklenght;
   
-  volatile uint8_t *PortRegister_C1;
-  volatile uint8_t *PortRegister_C2;
-  volatile uint8_t *PortRegister_C4;
-  volatile uint8_t *PortRegister_S2;
-  volatile uint8_t *PortRegister_BDH;
+  // Teensy 3.0 & 3.1 & 3.2 & 3.5 & 3.6
+  //#if defined (__MK20DX128__) || defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MKL26Z64__) || defined(__MK66FX1M0__)
+    volatile uint8_t *PortRegister_C1;
+    volatile uint8_t *PortRegister_C2;
+    volatile uint8_t *PortRegister_C4;
+    volatile uint8_t *PortRegister_S2;
+    volatile uint8_t *PortRegister_BDH;
+  //#elif defined(__IMXRT1062__) // Teensy 4.0 & 4.1
+    volatile uint32_t *PortRegister_LPUART_STAT;
+    volatile uint32_t *PortRegister_LPUART_BAUD;
+    volatile uint32_t *PortRegister_LPUART_CTRL;
+  //#endif
   
   #define lin1x 1
   #define lin2x 2
@@ -46,7 +58,7 @@ private:
   int addrParity(int PID);
   volatile byte dataChecksum (volatile byte* message, int length, uint16_t sum);
   void write(byte PID, byte* message, int length, int checksumtype = 1);
-  int read(byte PID, byte* message, int length, int checksumtype = 1);
+  int read(byte PID, byte message[], int length, int checksumtype = 1);
 };
 
 #endif
